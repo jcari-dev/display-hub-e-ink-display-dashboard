30b7758bd4d6dba6111925d9c937f39df186aadd
Jorge Caridad <65676916+jcari-dev@users.noreply.github.com>
2025-01-14 17:15:30 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,31 +1,31 @@
-if ! command -v docker &> /dev/null
-then
+#!/bin/bash
+
+if ! command -v docker &> /dev/null; then
     echo "Docker is not installed. Please install Docker and try again."
     exit 1
 fi
 
-if ! docker compose version &> /dev/null
-then
-    echo "Docker Compose is not installed. Please install Docker Compose (plugin) and try again."
+if ! docker compose version &> /dev/null; then
+    echo "Docker Compose (plugin) is not installed. Please install it and try again."
     exit 1
 fi
 
 REPO_URL="https://github.com/jcari-dev/display-hub-e-ink-display-dashboard.git"
-REPO_NAME=$(basename "$REPO_URL" .git) 
+REPO_NAME=$(basename "$REPO_URL" .git)
 
 echo "Cloning repository..."
-git clone "$REPO_URL"
-
-if [ -d "$REPO_NAME" ]; then
-    cd "$REPO_NAME" || exit
+if git clone "$REPO_URL"; then
+    cd "$REPO_NAME" || { echo "Failed to navigate to repository directory. Exiting."; exit 1; }
 else
     echo "Failed to clone repository. Exiting."
     exit 1
 fi
 
 echo "Building and starting containers..."
-docker compose up --build -d
-
-DEVICE_IP=$(hostname -I | awk '{print $1}')
-
-echo "Installation complete. Access the web GUI at http://$DEVICE_IP"
+if docker compose up --build -d --privileged --device=/dev/gpiomem; then
+    DEVICE_IP=$(hostname -I | awk '{print $1}')
+    echo "Installation complete. Access the web GUI at http://$DEVICE_IP"
+else
+    echo "Failed to start Docker Compose. Exiting."
+    exit 1
+fi
