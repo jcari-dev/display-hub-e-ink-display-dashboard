29e7f2bf191d049a3ce68d60286fb76e6433c13e
Jorge Caridad <65676916+jcari-dev@users.noreply.github.com>
2025-01-14 12:20:58 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,24 @@
-#!/bin/bash
-
-# Ensure Docker is installed
 if ! command -v docker &> /dev/null
 then
     echo "Docker is not installed. Please install Docker and try again."
     exit 1
 fi
 
-# Ensure Docker Compose is installed
-if ! docker compose version &> /dev/null && ! command -v docker-compose &> /dev/null
-then
+if docker compose version &> /dev/null; then
+    COMPOSE_COMMAND="docker compose"
+elif command -v docker-compose &> /dev/null; then
+    COMPOSE_COMMAND="docker-compose"
+else
     echo "Docker Compose is not installed. Please install Docker Compose and try again."
     exit 1
 fi
 
-# Clone the repository
 REPO_URL="https://github.com/jcari-dev/display-hub-e-ink-display-dashboard.git"
 REPO_NAME=$(basename "$REPO_URL" .git) 
 
 echo "Cloning repository..."
 git clone "$REPO_URL"
 
-# Navigate to the cloned repository directory
 if [ -d "$REPO_NAME" ]; then
     cd "$REPO_NAME" || exit
 else
@@ -29,11 +26,9 @@ else
     exit 1
 fi
 
-# Build and start the containers
 echo "Building and starting containers..."
-docker compose up --build -d || docker-compose up --build -d
+$COMPOSE_COMMAND up --build -d
 
-# Get the IP address of the device
 DEVICE_IP=$(hostname -I | awk '{print $1}')
 
 echo "Installation complete. Access the web GUI at http://$DEVICE_IP"
